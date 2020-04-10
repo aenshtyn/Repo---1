f63cbc57b3c488359ecfc3e655a2e655cef51326
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-10 12:18:02 +0300

    created first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Repo } from './repo';
 
 @Component({
   selector: 'app-root',
@@ -6,12 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-//   title = 'Repos';
-//   repo = 'https://github.com/aenshtyn/'
-// }
-repos:string[];
-
-constructor(){
-  this.repos = ['Watch finding Nemo', 'Buy cookies', 'Get new phone case']
-}
+  //   title = 'Repos';
+  //   repo = 'https://github.com/aenshtyn/'
+  // }
+  repos:Repo[] = [
+    {id:1, name:'Watch finding Nemo', link: "https://github.com/aenshtyn/"},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
 }
